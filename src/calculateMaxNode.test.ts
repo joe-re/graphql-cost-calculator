5375bc5f65d75b7dcc336774b65b88bfb3ee2aad
joe-re <joe.tialtngo@gmail.com>
2023-09-10 08:09:45 +0900

    add mutation test

--- src/calculateMaxNode.test.ts
+++ src/calculateMaxNode.test.ts
@@ -6,6 +6,9 @@ const typeDefs = `#graphql
     item: Item
     items(first: Int!): ItemConnection
   }
+  type Mutation {
+    updateItem(id: ID!): Item
+  }
   type Item {
     id: ID!
     likedUsers(first: Int!): UserConnection
@@ -46,57 +49,80 @@ const schema = makeExecutableSchema({
 });
 
 describe('calculateMaxNode', () => {
-  it('should calculate the max node count with considering a first argument', () => {
-    const query = `
-      query {
-        item { # 1pt
-          id
-        }
-        items(first: 10) { # 10pt
-          edges {
-            node {
-              id
-              likedUsers(first: 10) { # 10 * 10 = 100 pt
-                edges {
-                  cursor
-                  node {
-                    id
+  describe('query', () => {
+    it('should calculate the max node count with considering a first argument', () => {
+      const query = `
+        query {
+          item { # 1pt
+            id
+          }
+          items(first: 10) { # 10pt
+            edges {
+              node {
+                id
+                likedUsers(first: 10) { # 10 * 10 = 100 pt
+                  edges {
+                    cursor
+                    node {
+                      id
+                    }
                   }
                 }
               }
             }
           }
         }
-      }
-    `
-    const result = calculateMaxNode(schema, query)
-    expect(result).toBe(111)
+      `
+      const result = calculateMaxNode(schema, query)
+      expect(result).toBe(111)
+    })
+
+    it('should calculate the max node count with considering a last argument', () => {
+      const query = `
+        query {
+          item { # 1pt
+            id
+          }
+          items(last: 10) { # 10pt
+            edges {
+              node {
+                id
+                likedUsers(last: 10) { # 10 * 10 = 100 pt
+                  edges {
+                    cursor
+                    node {
+                      id
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `
+      const result = calculateMaxNode(schema, query)
+      expect(result).toBe(111)
+    })
   })
 
-  it('should calculate the max node count with considering a last argument', () => {
-    const query = `
-      query {
-        item { # 1pt
-          id
-        }
-        items(last: 10) { # 10pt
-          edges {
-            node {
-              id
-              likedUsers(last: 10) { # 10 * 10 = 100 pt
-                edges {
-                  cursor
-                  node {
-                    id
-                  }
+  describe('mutation', () => {
+    it ('should calculate the max node count with considering a first argument', () => {
+      const query = `
+        mutation {
+          updateItem(id: "1") { # 1pt
+            id
+            likedUsers(first: 10) { # 10 pt
+              edges {
+                cursor
+                node {
+                  id
                 }
               }
             }
           }
-        }
-      }
-    `
-    const result = calculateMaxNode(schema, query)
-    expect(result).toBe(111)
+        }`
+      const result = calculateMaxNode(schema, query)
+      expect(result).toBe(11)
+    })
   })
 })
